1ac8b6efc8deda3c715ca07dd9822539a6145bf1
Dell <Dell@DESKTOP-RNQOE3H>
2022-01-14 18:46:19 +0545

    Add project files.

---
+++ learning mvc/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace learning_mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.firstname = "learning mvc asp.net";
+            ViewBag.lastname = "from broadway infosys and it4 solutions";
+            ViewBag.address = "kathmandu";
+
+
+            return View();
+        }
+
+        
+    }
+}
